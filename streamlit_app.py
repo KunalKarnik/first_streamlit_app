c90bb1b6d4a2fea25a981eda40bdbf6882e7c9a0
Kunal A. Karnik <kunalkarnik95@gmail.com>
2023-04-16 00:49:14 -0500

    Update streamlit_app.py
    bugfix assignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+c = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
@@ -14,6 +14,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Strawberries', 'Banana'])
+fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Strawberries', 'Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
