055e40550d083448513abb7da619a77b003946d9
Kunal A. Karnik <kunalkarnik95@gmail.com>
2023-07-10 14:14:20 -0500

    Update view

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,17 @@
 
 import streamlit
-import pandas
+import pandas as pd
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.header('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.header('🐔 Hard-Boiled Free-Range Egg')
+streamlit.header('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Strawberries', 'Banana'])
